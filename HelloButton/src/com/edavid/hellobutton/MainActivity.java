d7fb77a5683a2c07d57cdaa271644fda24e5cd7a
bede6362 <bede6362@EB-OR1009018.ad.francetelecom.fr>
2013-12-09 17:12:02 +0100

    UPDATE

--- HelloButton/src/com/edavid/hellobutton/MainActivity.java
+++ HelloButton/src/com/edavid/hellobutton/MainActivity.java
@@ -1,9 +1,11 @@
 package com.edavid.hellobutton;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -13,14 +15,29 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		editText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+		    @Override
+		    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+		        if (actionId == EditorInfo.IME_ACTION_DONE) {
+		    		changeMessage();
+		            return true;
+		        }
+		        return false;
+		    }
+		});
 	}
 
-	public void sendMessage(View view) {
+	private void changeMessage() {
 		EditText editText = (EditText) findViewById(R.id.edit_message);
-		String message = editText.getText().toString();
 		TextView textView = (TextView) findViewById(R.id.text_message);
-		textView.setText(message);
-		
+		textView.setText(editText.getText().toString());
+		editText.setText("");
+	}
+	
+	public void sendMessage(View view) {
+		changeMessage();
 	}
 	
 	@Override
