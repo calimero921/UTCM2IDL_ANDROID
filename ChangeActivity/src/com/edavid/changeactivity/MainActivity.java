e237ed196e8e63cb6e1fe4a0a053bdee997e9d70
bede6362 <bede6362@EB-OR1009018.ad.francetelecom.fr>
2013-12-10 14:03:15 +0100

    update

--- ChangeActivity/src/com/edavid/changeactivity/MainActivity.java
+++ ChangeActivity/src/com/edavid/changeactivity/MainActivity.java
@@ -2,14 +2,43 @@ package com.edavid.changeactivity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
+	public final static String EXTRA_SELECTED = "com.edavid.changeactivity.SELECTEDVALUE";
+	ListView listView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		listView = (ListView)findViewById(R.id.listitem);
+		
+		String[] values = new String[] {"A","B","C","D","E","F","G","H","I",
+										"J","K","L","M","N","O","P","Q","R",
+										"S","T","U","V","W","X","Y","Z"};
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item_layout, R.id.item_text, values);
+		
+		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Intent intent = new Intent(MainActivity.this, ItemDisplayActivity.class);
+				intent.putExtra(EXTRA_SELECTED, (String)parent.getItemAtPosition(position));
+				startActivity(intent);
+			}
+		});
+		
 	}
 
 	@Override
@@ -18,5 +47,4 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
 }
